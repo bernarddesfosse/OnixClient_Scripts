d9c184079c2cf66dca2a742cad80d888745ac38d
bernarddesfosse <61179492+bernarddesfosse@users.noreply.github.com>
2022-11-20 23:23:19 -0500

    Update client.lua

--- AutoComplete/library/client.lua
+++ AutoComplete/library/client.lua
@@ -7,7 +7,7 @@
 ---@field mcversion string The minecraft version
 client = client or {}
 
----Sends a client notification
+---Sends a suspicious client notification
 ---@param text string The text in the notification
 function client.notification(text) end
 
